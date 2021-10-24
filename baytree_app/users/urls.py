d4bd76b75775c8aa57fb79e8ccf772bf1679c260
Jake Kim <kimjakek@sfu.ca>
2021-10-24 15:26:28 -0700

    Add api to retrieve user data

--- baytree_app/users/urls.py
+++ baytree_app/users/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import MentorViews
 
 urlpatterns = [
-    path('mentors/', MentorViews.as_view()),
     path('mentors/<int:id>', MentorViews.as_view())
 ]
--- baytree_app/users/views.py
+++ baytree_app/users/views.py
@@ -2,18 +2,16 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import MentorSerializer
 from .models import MentorUser
+from .permissions import *
 
 class MentorViews(APIView):
+    permission_classes = [IsOwner]
 
     def get(self, request, id=None):
-        if id:
-            item = MentorUser.objects.get(user_id=id)
-            serializer = MentorSerializer(item)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-
-        items = MentorUser.objects.all()
-        serializer = MentorSerializer(items, many=True)
+        item = MentorUser.objects.get(user_id=id)
+        serializer = MentorSerializer(item)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
