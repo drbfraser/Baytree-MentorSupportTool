8bd610b5e64f3f4c74f6f7421b080b6d190b8dbc
KRA42 <kra42@sfu.ca>
2021-11-05 21:36:18 -0700

    session serializer fixes

--- baytree_app/sessions/serializers.py
+++ baytree_app/sessions/serializers.py
@@ -8,10 +8,10 @@ class SessionSerializer(serializers.ModelSerializer):
         fields = ['mentor', 'mentee', 'clock_in', 'clock_out', 'notes']
 
     def create(self, validated_data):
-        # Once the request data has been validated, we can create a todo item instance in the database
         return MentorSession.objects.create(
-            mentor=validated_data.get('text'),
-            mentee=validated_data.get('text'),
-            clock_in=validated_data.get('text'),
-            clock_out=validated_data.get('text')
+            mentor=validated_data.get('mentor'),
+            mentee=validated_data.get('mentee'),
+            clock_in=validated_data.get('clock_in'),
+            clock_out=validated_data.get('clock_out'),
+            notes=validated_data.get('notes')
         )
