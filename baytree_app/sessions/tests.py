c94ad4622d6cf365499da8da7756d57228295881
KRA42 <kra42@sfu.ca>
2021-11-05 18:23:42 -0700

    remove test

--- baytree_app/sessions/tests.py
+++ baytree_app/sessions/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
 
-from rest_framework.test import APIRequestFactory
 
 # Create your tests here.
-factory = APIRequestFactory()
-request = factory.post('/sessions/', {'title': 'new idea'}, format='json')
