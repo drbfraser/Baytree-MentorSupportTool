b239b18e6bba6339bac8873df9052dca835b8acf
Jake Kim <kimjakek@sfu.ca>
2021-11-08 05:51:06 -0800

    Improve design of goals page

--- frontend/src/Components/Goals.tsx
+++ frontend/src/Components/Goals.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react'
+
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 import NoteCard from './Notecard';
+import GoalsStatistics from './GoalsStatistics';
 
 export default function Goals() {
     const [goals, setGoals] = useState([]);
+    const [tabValue, setTabValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent<Element, Event>, newValue: number) => {
+      setTabValue(newValue);
+    };
 
     useEffect(() => {
       fetch('http://localhost:8000/goals/', {
@@ -32,14 +43,20 @@ export default function Goals() {
     }
     */
     return (
-      <Container>
-        <Grid container spacing={3}>
+      <Container maxWidth = "lg" sx = {{mt: 10}}>
+        <Tabs value={tabValue} onChange={handleChange} centered sx = {{mb: 3}}>
+            <Tab label="Active" />
+            <Tab label="Completed" />
+            <Tab label="All" />
+        </Tabs>
+        <Grid container spacing={3} sx = {{bgcolor: "white", p: 5, mb: 3, ml: 1, boxShadow: 2, borderRadius: 5}} style = {{height: "55vh"}}>
           {goals.map(goals => (
             <Grid item xs={12} md={6} lg={4}>
-            <NoteCard />
+                <NoteCard />
             </Grid>
           ))}
         </Grid>
+        <GoalsStatistics />
       </Container>
     )
 }
