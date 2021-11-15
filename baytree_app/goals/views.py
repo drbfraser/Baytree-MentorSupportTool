df0382587d9cbcef07ce228a0bc9ed8b6ccd436c
Herman Luo <hjl20@sfu.ca>
2021-11-15 11:33:52 -0800

    Rename GoalSerializer and add get/post methods

--- baytree_app/goals/views.py
+++ baytree_app/goals/views.py
@@ -1,18 +1,32 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
-from .serializers import GoalsSerializer
+from .serializers import GoalSerializer
 from .models import Goal
 from .permissions import *
 
+#adapted from https://stackabuse.com/creating-a-rest-api-with-django-rest-framework/
 
 class GoalViews(APIView):
 
     permission_classes = [IsOwner]
 
     def get(self, request, id=None):
+        if id:
+            item = Goal.objects.get(id=id)
+            serializer = GoalSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            
         item = Goal.objects.all()
-        serializer = GoalsSerializer(item, many=True)
+        serializer = GoalSerializer(item, many=True)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = GoalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
