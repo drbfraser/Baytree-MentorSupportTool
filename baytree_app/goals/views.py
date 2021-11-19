8171dacfc517523750eb8184734e2342fe2d641e
Herman Luo <hjl20@sfu.ca>
2021-11-19 13:53:57 -0800

    Remove old import

--- baytree_app/goals/views.py
+++ baytree_app/goals/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import filters
 from rest_framework import generics
@@ -18,7 +17,6 @@ class GoalViews(generics.ListAPIView):
     serializer_class = GoalSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['mentee', 'title', 'date', 'goal_review_date', 'status']
-
 
     def post(self, request):
         serializer = GoalSerializer(data=request.data)
