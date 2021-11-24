e37c77a5a9b88ef474890455ba640456d9a2725d
Yanze Zheng <zhengbill7@gmail.com>
2021-11-23 23:49:31 -0800

    remove session for admin

--- baytree_app/baytree_app/settings.py
+++ baytree_app/baytree_app/settings.py
@@ -94,17 +94,22 @@ WSGI_APPLICATION = 'baytree_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Baytree',
+#         'USER': 'Baytree',
+#         'PASSWORD': 'Baytree123',
+#         'HOST': 'db',  
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Baytree',
-        'USER': 'Baytree',
-        'PASSWORD': 'Baytree123',
-        'HOST': 'db',  
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- baytree_app/sessions/admin.py
+++ baytree_app/sessions/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import MentorSession
 
-admin.site.register(MentorSession)
+# admin.site.register(MentorSession)
