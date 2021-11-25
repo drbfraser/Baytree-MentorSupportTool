5e98e8fc02033525236dadee3d15e83ff2d5b216
Jake Kim <kimjakek@sfu.ca>
2021-11-25 15:06:44 -0800

    Display sessions on scheduler

--- frontend/src/Components/Scheduler.tsx
+++ frontend/src/Components/Scheduler.tsx
@@ -1,5 +1,6 @@
+import {useState, useEffect} from 'react'
+
 import {Calendar, dateFnsLocalizer} from 'react-big-calendar'
-import moment from 'moment'
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
@@ -16,26 +17,52 @@ export default function Scheduler(){
     const locales = {
         'en-US': enUS,
       }
-      const endOfHour = (date: Date): Date => addHours(startOfHour(date), 1)
-      const now = new Date()
-      const start = endOfHour(now)
-      const end = addHours(start, 2)
-      // The types here are `object`. Strongly consider making them better as removing `locales` caused a fatal error
-      const localizer = dateFnsLocalizer({
+    const endOfHour = (date: Date): Date => addHours(startOfHour(date), 1)
+    const now = new Date()
+    const start = endOfHour(now)
+    const end = addHours(start, 2)
+    const localizer = dateFnsLocalizer({
         format,
         parse,
         startOfWeek,
         getDay,
         locales,
-      })
+    })
+    const [sessionList, setSessionList] = useState([] as any[]);
+    var events: any = [];
+    
+    useEffect(() => {
+    fetch('http://localhost:8000/sessions/', {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    })
+    .then (response => response.json())
+    .then (data => {
+        data.map((values: any, index: number) => {
+            var event = {
+                title: "Session " + (index+1),
+                start: new Date(values.clock_in),
+                end: new Date(values.clock_out),
+                allDay: false
+            }
+            events = [...events, event];
+        })
+        setSessionList(events);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    }, []);
 
     return (
         <div>
             <Card sx = {{border: 0.1, boxShadow: 2, borderRadius: 5, p: 3, mb: 5}} style = {{height: "55vh"}}>
                 <Calendar
                 localizer={localizer}
-                startAccessor="start"
-                endAccessor="end"
+                allDayAccessor = "false"
+                events = {sessionList}
                 />
             </Card>
         </div>
