8e2f6596429538f2cd4fbf2b86aea95b4d257647
KRA42 <kra42@sfu.ca>
2021-12-03 19:16:57 -0800

    Fixed minor issues in questionnaires.urls

--- baytree_app/questionnaires/urls.py
+++ baytree_app/questionnaires/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import QuestionnaireView
-from . import views
 
 
 urlpatterns = [
     path('', QuestionnaireView.as_view()),
     path('<int:id>', QuestionnaireView.as_view()),
-    path('get-report/', views.get_report),
-    path('get-report/<str:id>', views.get_report)
+    path('get_questionnaire/', QuestionnaireView.get_questionnaire),
+    path('get_questionnaire/<str:id>', QuestionnaireView.get_questionnaire)
 ]
