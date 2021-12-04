3b1cb29b114d11e25099af8124b2db82f590829a
KRA42 <kra42@sfu.ca>
2021-12-03 19:10:51 -0800

    url for retrieving questionnaires from views. (get-report)
    the default url retrieves from sql database

--- baytree_app/questionnaires/urls.py
+++ baytree_app/questionnaires/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import QuestionnaireView
+from . import views
+
 
 urlpatterns = [
     path('', QuestionnaireView.as_view()),
     path('<int:id>', QuestionnaireView.as_view()),
+    path('get-report/', views.get_report),
+    path('get-report/<str:id>', views.get_report)
 ]
