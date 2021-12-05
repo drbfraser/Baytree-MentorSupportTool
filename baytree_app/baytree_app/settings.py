cfda6383c231ccb454111086781234877ae95025
Yanze Zheng <zhengbill7@gmail.com>
2021-12-05 01:56:26 -0800

    fix something

--- baytree_app/baytree_app/settings.py
+++ baytree_app/baytree_app/settings.py
@@ -97,17 +97,22 @@ WSGI_APPLICATION = 'baytree_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Baytree',
+#         'USER': 'Baytree',
+#         'PASSWORD': 'Baytree123',
+#         'HOST': 'db',  
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Baytree',
-        'USER': 'Baytree',
-        'PASSWORD': 'Baytree123',
-        'HOST': 'db',  
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
