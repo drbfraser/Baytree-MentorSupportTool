31cc962fa9ec6bb554183cb13ede7e9104bd12d9
Jake Kim <kimjakek@sfu.ca>
2021-12-04 20:52:18 -0800

    Removed resources file

--- frontend/src/Components/Profile.tsx
+++ frontend/src/Components/Profile.tsx
@@ -1,8 +1,24 @@
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+
 const Profile = () => {
     return (
-      <div>
-        <h3>{localStorage.getItem('firstname')} {localStorage.getItem('lastname')}</h3>
-      </div>
+      <Container 
+        maxWidth = "md" 
+        sx = {{border: 0.1, boxShadow: 2, borderRadius: 5, pt: 2, mt: 3}}
+        style={{ background: '#ffffff' }}>
+        <Typography component="h2" variant="h6" color="text.secondary" gutterBottom>
+          {localStorage.getItem('firstname')} {localStorage.getItem('lastname')}
+        </Typography>
+        <Divider />
+
+
+
+
+
+        
+      </Container>
     )
   };
   
