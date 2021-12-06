26787fb9d3afe9934fdfb08434939f3beca70106
Yanze Zheng <zhengbill7@gmail.com>
2021-12-05 20:14:09 -0800

    fix reset password problem

--- baytree_app/baytree_app/settings.py
+++ baytree_app/baytree_app/settings.py
@@ -200,4 +200,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'zhengbill7@gmail.com'
-EMAIL_HOST_PASSWORD = 'Zyz576220168'
+EMAIL_HOST_PASSWORD = 'aozfymvfkkxswoqx'
