48011e2f9828a170725dc8aa0f25b6f271dc5adb
Noel Riopel <nriopel@sfu.ca>
2022-02-05 22:32:53 -0800

    Add primary colour constant for admin portal

--- admin-frontend/context/constants.tsx
+++ admin-frontend/context/constants.tsx
@@ -2,3 +2,4 @@ export const MOBILE_BREAKPOINT = "61.25rem";
 export const SIDEBAR_WIDTH = "14rem";
 export const TOPBAR_HEIGHT = "5rem";
 export const BODY_BACKGROUND = "#F9F3EB";
+export const BAYTREE_PRIMARY_COLOR = "#5AB031";
