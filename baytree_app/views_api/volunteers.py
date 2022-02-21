59f87d4c33788550a950b63dc6435a7c1fb70f36
Noel Riopel <nriopel@sfu.ca>
2022-02-20 23:32:08 -0800

    Add docstring for get_volunteers views api function

--- baytree_app/views_api/volunteers.py
+++ baytree_app/views_api/volunteers.py
@@ -5,7 +5,20 @@ volunteers_base_url = views_base_url + "contacts/volunteers/"
 
 volunteerFields = ["Forename", "Surname", "PersonID", "Email"]
 volunteerTranslateFields = ["firstname", "surname", "personId", "email"]
+
+
 def get_volunteers(id: str = None, limit: int = 5, offset: int = 0):
+    """
+    Gets volunteers from Views API.
+    If an id argument is provided, the volunteer with a matching PersonId will be returned.
+    The limit and offset parameters are used to implement pagination.
+    The limit parameter determines how many vounteers to return from the Views API.
+    The offset parameter determines which volunteer to start at when asking for
+    a number of volunteers from Views when using the limit parameter.
+    So, if limit = 5 and offset = 5, this would say: "give me 5 volunteers,
+    but skip the first 5 in the total volunteers returned by the Views API."
+    """
+
     if id != None:
         response = requests.get(
             volunteers_base_url + id,
