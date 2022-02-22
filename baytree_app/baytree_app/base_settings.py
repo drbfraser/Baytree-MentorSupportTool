62a45ec45f6efd19a5f3021a43db7bd71b74b925
Noel Riopel <nriopel@sfu.ca>
2022-02-22 15:45:31 -0800

    Refactor Django setting modules and introduce environment variables

--- baytree_app/baytree_app/base_settings.py
+++ baytree_app/baytree_app/base_settings.py
@@ -21,13 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%bw7ei*$^qt%1tappq&+r)l@!6p&dg#=80400^^+--k16tqt8q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
 ALLOWED_HOSTS = []
 
 
@@ -188,9 +181,3 @@ SIMPLE_JWT = {
 
 
 SITE_ID = 1
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'blabla@gmail.com'
-EMAIL_HOST_PASSWORD = 'nbcvjhsdkltwertopj'
