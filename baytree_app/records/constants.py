9ac944adb25b8cce24eb89b413fa16ead0b0837f
ssanei <ssanei@sfu.ca>
2022-03-07 20:05:22 -0800

    coreecting the Get request url from staff to volunteer

--- baytree_app/records/constants.py
+++ baytree_app/records/constants.py
@@ -1,5 +1,5 @@
 import os
 
-views_base_url = 'https://app.viewsapp.net/api/restful/contacts/staff/'
+views_base_url = 'https://app.viewsapp.net/api/restful/contacts/volunteers/'
 views_username = os.environ["VIEWS_USERNAME"]
 views_password = os.environ["VIEWS_PASSWORD"]
