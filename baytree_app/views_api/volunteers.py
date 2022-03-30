910949ddae6cfd80eb6c7d88e8928792c0d41304
Noel Riopel <nriopel@sfu.ca>
2022-03-30 15:41:06 -0700

    Add search by email to views endpoint

--- baytree_app/views_api/volunteers.py
+++ baytree_app/views_api/volunteers.py
@@ -18,19 +18,36 @@ Mentors are also considered as "Staff", but not all staff are mentors, so we sho
 staff members since we could retrieve members that aren't actually mentors.
 """
 
-def get_volunteers(id: str = None, limit: int = 5, offset: int = 0):
+def get_volunteers(id: str = None, limit: int = 5, offset: int = 0, searchEmail: str = None):
     """
     Gets volunteers from Views API.
     If an id argument is provided, the volunteer with a matching PersonId will be returned.
     The limit and offset parameters are used to implement pagination.
     The limit parameter determines how many vounteers to return from the Views API.
     The offset parameter determines which volunteer to start at when asking for
+    The searchEmail parameter filters for volunteers by email.
     a number of volunteers from Views when using the limit parameter.
     So, if limit = 5 and offset = 5, this would say: "give me 5 volunteers,
     but skip the first 5 in the total volunteers returned by the Views API."
     """
 
-    if id != None:
+    if searchEmail != None:
+        response = requests.get(
+            volunteers_base_url + "/search?Email=" + searchEmail,
+            auth=(views_username, views_password))
+
+        parsed = xmltodict.parse(response.text)
+
+        # Make sure the volunteers are wrapped in a list, if there is a single volunteer
+        if not isinstance(parsed["contacts"]["volunteers"]["volunteer"], list):
+            parsed["contacts"]["volunteers"]["volunteer"] = [parsed["contacts"]["volunteers"]["volunteer"]]
+
+        volunteers = [{volunteerTranslateFields[i]: volunteer[field] for i, field in enumerate(volunteerFields)}
+                      for volunteer in parsed["contacts"]["volunteers"]["volunteer"]]
+
+        return {"total": parsed["contacts"]["volunteers"]["@count"], "data": volunteers}
+
+    elif id != None:
         response = requests.get(
             volunteers_base_url + id,
             auth=(views_username, views_password))
@@ -70,8 +87,11 @@ def get_volunteers_endpoint(request):
     to return its response to the client.
     """
     id = request.GET.get('id', None)
+    searchEmail = request.GET.get('searchEmail', None)
 
-    if id != None:
+    if searchEmail != None:
+        response = get_volunteers(searchEmail=searchEmail)
+    elif id != None:
         response = get_volunteers(id)
     else:
         response = get_volunteers(limit=request.GET.get(
