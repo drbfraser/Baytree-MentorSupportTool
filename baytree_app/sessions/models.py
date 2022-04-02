46052f3a94d0a4412dbc9b3dc01f9a7a1fcbdf52
Noel Riopel <nriopel@sfu.ca>
2022-04-01 18:08:19 -0700

    Get monthly expected sessions model and CRUD endpoints

--- baytree_app/sessions/models.py
+++ baytree_app/sessions/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -7,8 +8,12 @@ from users.models import CustomUser
 class MentorSession(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    mentor = models.ForeignKey(CustomUser, related_name="sessions_mentor", on_delete=models.PROTECT, null=True)
-    mentee = models.ForeignKey(CustomUser, related_name="sessions_mentee", on_delete=models.PROTECT, null=True)
+    mentor = models.ForeignKey(
+        CustomUser, related_name="sessions_mentor", on_delete=models.PROTECT, null=True
+    )
+    mentee = models.ForeignKey(
+        CustomUser, related_name="sessions_mentee", on_delete=models.PROTECT, null=True
+    )
     attended_by_mentor = models.BooleanField(blank=False, default=True)
     attended_by_mentee = models.BooleanField(blank=False, default=True)
     clock_in = models.DateTimeField(blank=True, null=True)
@@ -20,3 +25,17 @@ class MentorSession(models.Model):
         return result
 
 
+class MonthlyExpectedSessionCounts(models.Model):
+    year = models.IntegerField(primary_key=True)
+    january_count = models.IntegerField(default=0)
+    february_count = models.IntegerField(default=0)
+    march_count = models.IntegerField(default=0)
+    april_count = models.IntegerField(default=0)
+    may_count = models.IntegerField(default=0)
+    june_count = models.IntegerField(default=0)
+    july_count = models.IntegerField(default=0)
+    august_count = models.IntegerField(default=0)
+    september_count = models.IntegerField(default=0)
+    october_count = models.IntegerField(default=0)
+    november_count = models.IntegerField(default=0)
+    december_count = models.IntegerField(default=0)
--- baytree_app/sessions/urls.py
+++ baytree_app/sessions/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from sessions.models import MonthlyExpectedSessionCounts
+from sessions.permissions import AdminPermissions
+
+from baytree_app.views import GenerateCrudEndpointsForModel
 from .views import SessionView, ViewsAppSessionView
 
 urlpatterns = [
-    path('', SessionView.as_view()),
-    path('<int:id>', SessionView.as_view()),
-    path('viewsapp/', ViewsAppSessionView.as_view())
+    path("", SessionView.as_view()),
+    path("<int:id>", SessionView.as_view()),
+    path("viewsapp/", ViewsAppSessionView.as_view()),
+    path(
+        "monthly-expected-session-counts",
+        GenerateCrudEndpointsForModel.as_view(
+            model=MonthlyExpectedSessionCounts, permission_classes=[AdminPermissions]
+        ),
+    ),
 ]
