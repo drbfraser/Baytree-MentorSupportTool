f59f11d4fee7decc729db4f7f1f789bd3922416f
Viet Hung Nguyen <vhn@sfu.ca>
2022-05-21 23:35:32 -0700

    Add build configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   server: {
     host: "0.0.0.0"
   },
