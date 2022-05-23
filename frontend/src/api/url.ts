797c211be537e19c08f6535ea72ec9cedee319c5
Viet Hung Nguyen <vhn@sfu.ca>
2022-05-22 22:09:10 -0700

    Add vite configuration and API change

--- frontend/src/api/url.ts
+++ frontend/src/api/url.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  (import.meta.env.DEV ? `http://${window.location.hostname}:8000` : "") + "/api";
+  (import.meta.env.DEV ? `http://${window.location.hostname}:8000` : "") +
+  "/api";
