040e89a58d8e6d144438edcb2df9c46fce2c20f5
catnip1032 <catherine_riopel@hotmail.com>
2022-05-23 18:03:41 -0700

    Create login modal for initial login data privacy message

---
+++ frontend/src/Components/shared/LoginModal.tsx
@@ -0,0 +1,57 @@
+import { Typography, Button } from "@mui/material";
+import useMobileLayout from "../../hooks/useMobileLayout";
+
+interface AddLoginModalProps {
+  onOutsideClick: () => void;
+}
+
+const LoginModal: React.FC<AddLoginModalProps> = (props) => {
+    const onMobileDevice = useMobileLayout();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Typography variant="h5" style={{ textAlign: "center" }}>
+      Our Data Protection Privacy statement explains how we look after your information and what we do with it, you can find a copy on our website or by asking a member of staff. We will save the information you provide on our systems, which are accessible to all our staff. Generally, the information you provide us will be treated as confidential amongst our staff, however, in certain circumstances, we may need to disclose some information to third parties (including, for example, the social services). By accessing our services, you confirm that you consent to the storage and use of your data.
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: onMobileDevice ? "column" : "row",
+          justifyContent: "space-around",
+          paddingTop: "3rem",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="error"
+          onClick={async () => {
+            //TODO: Login here  
+            console.log("Logging in")
+          }}
+          style={{ marginBottom: onMobileDevice ? "3rem" : "0" }}
+        >
+          Log out
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => {
+            console.log("cancelling the log in")
+            props.onOutsideClick();
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
