6d997553232764e0c87526cfb6b1b0b526497033
Viet Hung Nguyen <vhn@sfu.ca>
2022-05-31 23:58:14 -0700

    Fix delete questionnaire model

--- baytree_app/questionnaires/models.py
+++ baytree_app/questionnaires/models.py
@@ -1,26 +1,4 @@
 from django.db import models
-from users.models import CustomUser
 
 # Create your models here.
 
-
-class Questionnaire(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    mentor = models.ForeignKey(
-        CustomUser,
-        related_name="questionnaire_mentor",
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    mentee = models.ForeignKey(
-        CustomUser,
-        related_name="questionnaire_mentee",
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    inlines = []
-
-    def __str__(self):
-        result = str(self.id) + ", " + self.mentor.email + ", " + self.mentee.email
-        return result
