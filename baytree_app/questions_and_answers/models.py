4b4e7d6b402db6ff487e708af0fe87c79a56c41e
Viet Hung Nguyen <vhn@sfu.ca>
2022-06-02 17:34:03 -0700

    Delete QuestionAndAnswer model

--- baytree_app/questions_and_answers/models.py
+++ baytree_app/questions_and_answers/models.py
@@ -1,6 +1,1 @@
 from django.db import models
-
-
-class QuestionAndAnswer(models.Model):
-    question = models.CharField(max_length=500)
-    answer = models.CharField(max_length=500)
--- baytree_app/questions_and_answers/urls.py
+++ baytree_app/questions_and_answers/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import QuestionAndAnswerView
 
 urlpatterns = [
-    path('', QuestionAndAnswerView.as_view()),
-    path('<int:id>', QuestionAndAnswerView.as_view())
+    
 ]
