cd1033a23598314ffd9d52f6a8b25f740466539f
Catherine Riopel <criopel@sfu.ca>
2022-06-05 22:41:29 -0700

    Set type parameter to type onSaveDataRowsFunc
    Co-authored-by: Noel Riopel <nriopel@sfu.ca>

--- admin-frontend/components/shared/datagrid/datagridTypes.tsx
+++ admin-frontend/components/shared/datagrid/datagridTypes.tsx
@@ -28,10 +28,10 @@ export type PagedDataRows<DataRowType> = {
     ]*/;
 };
 
-export type onSaveDataRowsFunc = (
-  createdRows: DataRow[],
-  updatedRows: DataRow[],
-  deletedRows: DataRow[]
+export type onSaveDataRowsFunc<DataRowType> = (
+  createdRows: DataRowType[],
+  updatedRows: DataRowType[],
+  deletedRows: DataRowType[]
 ) => Promise<boolean>;
 
 export interface DataGridColumn {
@@ -44,6 +44,7 @@ export interface DataGridColumn {
   enableSearching?: boolean;
   keepColumnOnMobile?: boolean;
   expandableColumn?: boolean;
+  dataType?: string;
 }
 
 export type OnLoadColumnValueOptionsFunc = () => Promise<ValueOption[]>;
