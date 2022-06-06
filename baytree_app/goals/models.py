0c95e6a8a925f2edffa5c646d5873ab1089e0656
Viet Hung Nguyen <vhn@sfu.ca>
2022-06-06 14:50:22 -0700

    Update goal model

--- baytree_app/goals/models.py
+++ baytree_app/goals/models.py
@@ -9,12 +9,12 @@ class Goal(models.Model):
         RECALIBRATED = 'RECALIBRATED', "RECALIBRATED"
 
     mentor = models.ForeignKey(CustomUser, related_name="goal_mentor", on_delete=models.SET_NULL, null=True)
-    mentee = models.ForeignKey(CustomUser, related_name="goal_mentee", on_delete=models.SET_NULL, null=True)
+    mentee_id = models.IntegerField(null=True)
     title = models.CharField(max_length=100)
-    date = models.DateField()
+    creation_date = models.DateField()
     goal_review_date = models.DateField()
     last_update_date = models.DateTimeField(auto_now=True)
-    content = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
     status = models.CharField(
         max_length = 12,
         choices=Status.choices,
