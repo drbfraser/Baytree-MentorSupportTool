ff858d3f663e8aebbeb149ef7ad0bd6440be678b
Catherine Riopel <criopel@sfu.ca>
2022-06-13 15:49:11 -0700

    Create new path to enable answer set deletion

--- baytree_app/questionnaires/urls.py
+++ baytree_app/questionnaires/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_questionnaire, submit_answer_set
+from .views import get_questionnaire, submit_answer_set, delete_answer_set
 
 # /api/questionnaires/
 urlpatterns = [
     path('questionnaire/', get_questionnaire),
-    path('questionnaire/submit/', submit_answer_set)
+    path('questionnaire/delete/', delete_answer_set),
+    path('questionnaire/submit/', submit_answer_set),
 ]
