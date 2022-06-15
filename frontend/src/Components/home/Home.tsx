101c70bb6532963e067695e8c187011b5b1d3a0a
Viet Hung Nguyen <vhn@sfu.ca>
2022-06-15 11:10:43 -0700

    Remove react-big-calendar

--- frontend/src/Components/home/Home.tsx
+++ frontend/src/Components/home/Home.tsx
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import GoalList from "../goals/GoalsList";
 import MenteeInfo from "./MenteeInfo";
-import HomeScheduler from "./HomeScheduler";
+import Scheduler from "./Scheduler";
 import Statistics from "./Statistics";
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
     <Container maxWidth="lg">
       <Grid container spacing={4}>
         <Grid item xs={12} sm={12} md={12} lg={8}>
-          <HomeScheduler />
+          <Scheduler />
           <Statistics />
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={4}>
