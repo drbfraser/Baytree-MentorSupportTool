98e1fc15263016a18efed1e63a95dbd3bcd22b7e
Viet Hung Nguyen <vhn@sfu.ca>
2022-06-24 16:16:55 -0700

    Logout due to expired refresh token

--- frontend/src/hooks/useRefreshToken.ts
+++ frontend/src/hooks/useRefreshToken.ts
@@ -2,14 +2,15 @@ import { AxiosError, AxiosInstance } from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { refreshAccessToken } from "../api/auth";
+import { useAuth } from "../context/AuthContext";
 
 const useRefreshToken = (api: AxiosInstance) => {
   const navigate = useNavigate();
+  const { signOut } = useAuth();
   useEffect(() => {
     const interceptor = api.interceptors.response.use(
       (response) => response,
       async (error: AxiosError) => {
-        console.log("Intercepting...");
         const prevRequest = error.config;
         // Check for unauthorized error
         if (prevRequest && error.response?.status === 401) {
@@ -19,14 +20,17 @@ const useRefreshToken = (api: AxiosInstance) => {
           // Resend the request if refresh was sucessful
           if (refreshed) return api(prevRequest);
 
-          // Navigate back to login page for expired refresh token
-          else return navigate("/login");
+          // Log out and Navigate back to login page for expired refresh token
+          else {
+            await signOut();
+            return navigate("/login");
+          }
         }
         return error;
       }
     )
     return () => {
-      console.log("Ejecting...")
+      // Eject this interceptor when the private routes unmounted
       api.interceptors.response.eject(interceptor);
     }
   }, [])
