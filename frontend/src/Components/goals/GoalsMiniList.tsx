47be9502fe17058280a5ec707995420fd8589a58
Viet Hung Nguyen <vhn@sfu.ca>
2022-06-25 02:30:14 -0700

    Only expand one item in active goal summary

--- frontend/src/Components/goals/GoalsMiniList.tsx
+++ frontend/src/Components/goals/GoalsMiniList.tsx
@@ -1,17 +1,28 @@
 import { Alert, Box } from "@mui/material";
+import { useState } from "react";
 import useGoals from "../../hooks/useGoals";
 import Loading from "../shared/Loading";
 import GoalListItem from "./GoalListItem";
 
 const GoalsMiniList = () => {
   const { goals, loading, error } = useGoals({ limit: 3, active: true });
+  const [id, setId] = useState<number | undefined>();
 
   if (loading) return <Loading />;
   if (error) return <Alert severity="error">{error}</Alert>;
   if (goals.length === 0) return <Alert severity="info">All goals achieved!</Alert>;
 
   return <Box>
-    {goals.map((goal) => <GoalListItem key={goal.id} goal={goal} minified />)}
+    {goals.map((goal) => {
+      const expanded = id === goal.id;
+      const handleClick = () => expanded ? setId(undefined) : setId(goal.id)
+      return <GoalListItem 
+        key={goal.id} 
+        expanded={expanded}
+        handleClick={handleClick}
+        goal={goal} 
+        minified />
+    })}
   </Box>
 }
 
