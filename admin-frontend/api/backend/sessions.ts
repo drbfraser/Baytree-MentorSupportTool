c866da71510acd0cd0d98c0fdb67ef587f2d0029
Noel Riopel <nriopel@sfu.ca>
2022-06-27 01:03:55 -0700

    Add endpoint urls and admin portal venue service methods

--- admin-frontend/api/backend/sessions.ts
+++ admin-frontend/api/backend/sessions.ts
@@ -1,39 +1,35 @@
 /**
-  Endpoints for Mentor Sessions
+  Endpoints for Allowable session venues
 */
 
-import { generateBackendCrudFuncs } from "./base";
-import { API_BASE_URL } from "./url";
+import { PagedDataRows } from "../../components/shared/datagrid/datagridTypes";
+import { ApiOptions, backendGet, backendPost } from "./base";
 
-export interface SessionResponse {
-  created_at: string; // format: 2022-03-22T14:06:01.692237-07:00
-  updated_at: string; // format: 2022-03-22T14:06:01.692237-07:00
-  mentor: number; // mentor id
-  mentee: number; // mentee id
-  attended_by_mentor: boolean;
-  attended_by_mentee: boolean;
-  clock_in: Date;
-  clock_out: Date;
-  notes: string;
-  cancelled: boolean;
-  viewsSessionId: string; // Matching session id in views
+export interface Venue {
+  viewsVenueId: number;
 }
 
-export const sessionsBackendEndpoint = `${API_BASE_URL}/sessions/`;
+export const venuesBackendEndpoint = `sessions/venues/`;
 
-export const getSessions = async () => {
-  try {
-    const response = await fetch(`${sessionsBackendEndpoint}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
+export const getVenues = async (options?: ApiOptions) => {
+  const queryParams: Record<string, any> = {};
 
-    return (await response.json()) as SessionResponse[];
-  } catch {
-    return null;
+  if (options) {
+    const { limit, offset } = options;
+    if (limit) {
+      queryParams["limit"] = limit;
+    }
+    if (offset) {
+      queryParams["offset"] = offset;
+    }
   }
+
+  return await backendGet<Venue[] | PagedDataRows<Venue>>(
+    venuesBackendEndpoint,
+    queryParams
+  );
+};
+
+export const saveVenues = async (venueDataRows: Record<string, any>[]) => {
+  return await backendPost(venuesBackendEndpoint, venueDataRows);
 };
--- baytree_app/sessions/urls.py
+++ baytree_app/sessions/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
-from .views import ViewsAppSessionView
+from django.urls import include, path
+from .views import SessionView, VenueViewSet, ViewsAppSessionView
 
-from .views import SessionView, ViewsAppSessionView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"venues", VenueViewSet)
 
 urlpatterns = [
     path("", SessionView.as_view()),
     path("<int:id>", SessionView.as_view()),
     path("viewsapp/", ViewsAppSessionView.as_view()),
+    path("", include(router.urls)),
 ]
