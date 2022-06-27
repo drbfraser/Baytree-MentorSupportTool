1602c6bf7c804ef094366547622171dc5f9b0528
Noel Riopel <nriopel@sfu.ca>
2022-06-27 15:33:49 -0700

    Restore session service methods

--- admin-frontend/api/backend/sessions.ts
+++ admin-frontend/api/backend/sessions.ts
@@ -1,9 +1,43 @@
-/**
-  Endpoints for Allowable session venues
-*/
-
 import { PagedDataRows } from "../../components/shared/datagrid/datagridTypes";
 import { ApiOptions, backendGet, backendPost } from "./base";
+import { API_BASE_URL } from "./url";
+
+/**
+  Endpoints for Mentor Sessions and Allowable Venues
+*/
+
+export interface SessionResponse {
+  created_at: string; // format: 2022-03-22T14:06:01.692237-07:00
+  updated_at: string; // format: 2022-03-22T14:06:01.692237-07:00
+  mentor: number; // mentor id
+  mentee: number; // mentee id
+  attended_by_mentor: boolean;
+  attended_by_mentee: boolean;
+  clock_in: Date;
+  clock_out: Date;
+  notes: string;
+  cancelled: boolean;
+  viewsSessionId: string; // Matching session id in views
+}
+
+export const sessionsBackendEndpoint = `${API_BASE_URL}/sessions/`;
+
+export const getSessions = async () => {
+  try {
+    const response = await fetch(`${sessionsBackendEndpoint}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    return (await response.json()) as SessionResponse[];
+  } catch {
+    return null;
+  }
+};
 
 export interface Venue {
   viewsVenueId: number;
