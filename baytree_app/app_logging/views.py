e3402937976d601f57df4e5d1fb14ddb989fb38b
Catherine Riopel <criopel@sfu.ca>
2022-07-02 18:27:20 -0700

    Finish backend logging POST

--- baytree_app/app_logging/views.py
+++ baytree_app/app_logging/views.py
@@ -1,39 +1,41 @@
 import logging
-from urllib.request import Request
-from django.http import HttpResponse
 
-import requests
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
-# POST /api/logging/
-@api_view(("POST", ))
-def submit_log(request):
-    """
-    Submit the log to logging file
-    """
-    # Validate data existence
-    data = request.data
-    if data["logType"] is None or data["logMessage"] is None:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    
-    # Get a logger
-    logger = logging.getLogger(__name__)
+# POST api/logging
+class LoggingViews(generics.GenericAPIView):
+    permission_classes = [AllowAny]
 
-    # log message
-    logType = data["logType"]
-    logMessage = data["logMessage"]
-    if logType == "debug":
-        logger.debug(logMessage)
-    elif logType == "info":
-        logger.info(logMessage)
-    elif logType == "warning":
-        logger.warning(logMessage)
-    elif logType == "critical":
-        logger.critical(logMessage)
-    else:
-        logger.error(logMessage)
+    def post(self, request):
 
-    return Response(status=status.HTTP_200_OK)
+        # Validate data existence
+        data = request.data
+        if data["level"] is None or data["message"] is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        # Get a logger
+        logger = logging.getLogger("django")
+
+        # Log message
+        try:
+            # Create log message
+            logMessage = '[{0}] {1}: {2} \n {3}'
+            logMessage.format(data["timeStamp"], data["level"], data["message"], data["stackTrace"])
+            logLevel = data["level"]
+            if logLevel == "debug":
+                logger.debug(logMessage)
+            elif logLevel == "info":
+                logger.info(logMessage)
+            elif logLevel == "warning":
+                logger.warning(logMessage)
+            elif logLevel == "critical":
+                logger.critical(logMessage)
+            else:
+                logger.error(logMessage)
+
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
