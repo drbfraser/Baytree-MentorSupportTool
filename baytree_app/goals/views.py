6eec455bcb47f03527b186f6913a2847d258d554
Viet Hung Nguyen <vhn@sfu.ca>
2022-07-12 23:36:58 -0700

    Support goal categories for goal API

--- baytree_app/goals/views.py
+++ baytree_app/goals/views.py
@@ -17,31 +17,34 @@ class MentorGoalQuerySetMixin():
 
 # GET, POST /api/goals/
 class GoalListCreateAPIView(
-    MentorGoalQuerySetMixin,
-    ListCreateAPIView):
-    queryset = Goal.objects.all()
-    serializer_class = GoalSerializer
+  MentorGoalQuerySetMixin,
+  ListCreateAPIView):
+  queryset = Goal.objects.all()
+  serializer_class = GoalSerializer
 
-    def get_queryset(self, *args, **kwargs):
-      qs = super().get_queryset(*args, **kwargs)
-      active = self.request.query_params.get('active')
-      if active is not None: qs = qs.filter(status="IN PROGRESS")
-      completed = self.request.query_params.get('completed')
-      if completed is not None: qs = qs.filter(status="ACHIEVED")
-      return qs
+  def get_queryset(self, *args, **kwargs):
+    qs = super().get_queryset(*args, **kwargs)
+    active = self.request.query_params.get('active')
+    if active is not None: qs = qs.filter(status="IN PROGRESS")
+    completed = self.request.query_params.get('completed')
+    if completed is not None: qs = qs.filter(status="ACHIEVED")
+    return qs
 
-    def perform_create(self, serializer):
-        mentors = MentorUser.objects.filter(user_id=self.request.user.id)
-        if mentors is None: raise Http404() 
-        return serializer.save(mentor=mentors.first())
+  def perform_create(self, serializer):
+      mentors = MentorUser.objects.filter(user_id=self.request.user.id)
+      if mentors is None: raise Http404() 
+      return serializer.save(mentor=mentors.first(), options=self.request.data["options"])
 
 # GET, PUT, PATCH, DELETE /api/goals/<id>
 class GoalRetrieveUpdateDestroyAPIView(
-    MentorGoalQuerySetMixin,
-    RetrieveUpdateDestroyAPIView):
-    queryset = Goal.objects.all()
-    serializer_class = GoalSerializer
-    lookup_field = 'pk'
+  MentorGoalQuerySetMixin,
+  RetrieveUpdateDestroyAPIView):
+  queryset = Goal.objects.all()
+  serializer_class = GoalSerializer
+  lookup_field = 'pk'
+
+  def perform_update(self, serializer):
+    return serializer.save(options=self.request.data["options"])
 
 # GET /api/goals/options/
 class GoalOPtionsListView(ListAPIView):
