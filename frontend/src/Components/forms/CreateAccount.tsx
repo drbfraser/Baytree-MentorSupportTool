5bd0ad14fd2d8aa54bbbda5d40bc86da2951b619
Viet Hung Nguyen <vhn@sfu.ca>
2022-07-15 09:53:48 -0700

    Enforce strong pasword on frontend

--- frontend/src/Components/forms/CreateAccount.tsx
+++ frontend/src/Components/forms/CreateAccount.tsx
@@ -1,94 +1,88 @@
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ErrorIcon from '@mui/icons-material/Error';
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Alert, AlertTitle, Box, Button, TextField, Typography } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { FunctionComponent, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from "react-toastify";
-import { createMentorAccount } from "../../api/mentorAccount";
+import { createMentorAccount, userApi } from "../../api/mentorAccount";
+import PasswordValidation, { isValid } from "../shared/PasswordValidation";
 
-const ValidationMessage: FunctionComponent<{ label: string, validated: boolean }> = ({ label, validated }) => {
-  return <Box display="flex" alignItems="center">
-    {validated ? <CheckCircleIcon color="primary" fontSize="small" sx={{ m: 0.5 }} /> : <ErrorIcon color="error" fontSize="small" sx={{ m: 0.5 }} />}
-    <Typography variant="subtitle2" color={validated ? "primary" : "error"}>{label}</Typography>
-  </Box>
-}
-
-const CreateAccount = () => {
-  const navigate = useNavigate();
-  const [password, setPassword] = useState("");
-  const [passwordAgain, setPasswordAgain] = useState("");
+const useAccountCreationLink = () => {
+  const params = useParams();
+  const id = params.accountCreationId!;
   const [isPageLoading, setIsPageLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [hardError, setHardError] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  const openModal = () => {
-    setIsPageLoading(true);
-    setShowModal(true);
-  };
-
-  async function createAccount() {
-    const params = new URLSearchParams(window.location.search);
-    const accountCreationLinkId = params.get("id");
-
-    if (accountCreationLinkId) {
-      try {
-        setShowModal(false);
-        const apiRes = await createMentorAccount(
-          password,
-          accountCreationLinkId
-        );
-
-        if (apiRes.status === 200) {
-          toast.success("Account created successfully!");
-          navigate("/login");
-        } else if (apiRes.status === 410) {
-          toast.error(
-            "Link expired. Please contact an administrator for further assistance."
-          );
-        } else {
-          toast.error(
-            "Invalid link. Please contact an administrator for further assistance."
-          );
-        }
-      } catch {
-        toast.error(
-          "Failed to create mentor account. Please contact an administrator for further assistance."
-        );
-      } finally {
-        setIsPageLoading(false);
-      }
+  const handleError = (hardError: AxiosError, submitting?: boolean) => {
+    const code = hardError.response?.status;
+    if (!code || code === 500) setHardError("An error has occurred")
+    if (code === 401) setHardError("Invalid link");
+    else if (code === 410) setHardError("Link expired");
+    else if (submitting) {
+      toast.error(
+        "Failed to create an account. Please try again or contact an administrator for further assistance."
+      );
     }
   }
 
-  function modalAgreeClick() {
-    setShowModal(false);
-    setIsPageLoading(false);
-    createAccount();
+  useEffect(() => {
+    setIsPageLoading(true);
+    userApi.post('verifyAccountCreationLink', { id })
+      .catch(handleError).finally(() => setIsPageLoading(false));
+    return () => toast.dismiss();
+  }, []);
+
+  const createAccount = (password: string) => {
+    setIsPageLoading(true);
+    createMentorAccount(password, id)
+      .then(() => setSuccess(true))
+      .catch((error: AxiosError) => {
+        handleError(error, true);
+        setSuccess(false);
+      })
+      .finally(() => setIsPageLoading(false));
   }
 
-  function modalDisagreeClick() {
-    setShowModal(false);
-    setIsPageLoading(false);
+  return { isPageLoading, hardError, createAccount, success };
+}
+
+const CreateAccount = () => {
+  const { isPageLoading, hardError, createAccount, success } = useAccountCreationLink();
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  if (success) {
+    <Box width="100%" display="flex" flexDirection="column">
+      <Alert severity="success">Successfully created account!</Alert>
+      <Button sx={{ my: 2 }} variant="outlined" onClick={() => navigate("/login", { replace: true })}>
+        To login page
+      </Button>
+    </Box>
   }
 
-  const hasValidLength = password.length >= 8 && password.length <= 30;
-  const containsNumber = /\d/.test(password);
-  const containsLowercase = /[a-z]/.test(password);
-  const containsUppercase = /[A-Z]/.test(password);
-  const containsSpecial = /\W/.test(password);
-  const isMatch = password === passwordAgain;
-  const valid = hasValidLength && containsNumber && containsLowercase && containsUppercase && containsSpecial && isMatch;
+  if (hardError) return (
+    <Alert severity="error" sx={{ width: "100%" }}>
+      <AlertTitle>{hardError}</AlertTitle>
+      Please try again or contact the adminstrator for further assistance.
+    </Alert>
+  )
 
   return (
     <>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        openModal();
-      }}>
+      <form
+        style={{ width: "100%" }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setShowModal(true);
+        }}>
         <Typography width="100%" variant="h6">Create your account password</Typography>
         <TextField
           margin="normal"
@@ -115,28 +109,22 @@ const CreateAccount = () => {
           onChange={(e) => setPasswordAgain(e.target.value)}
         />
         {/* Validation fields */}
-        <Box padding={1} sx={{ backgroundColor: "rgba(0, 0, 0, 0.05)", my: 2 }}>
-          <ValidationMessage validated={hasValidLength} label="Contains 8 - 30 characters" />
-          <ValidationMessage validated={containsNumber} label="Contains at least one number" />
-          <ValidationMessage validated={containsLowercase} label="Contains at least one lowercase letter" />
-          <ValidationMessage validated={containsUppercase} label="Contains at least one uppercase letter" />
-          <ValidationMessage validated={containsSpecial} label="Contains at least one special character" />
-          <ValidationMessage validated={isMatch} label="Passwords match" />
-        </Box>
-        <Button
+        <PasswordValidation password={password} passwordAgain={passwordAgain} />
+        <LoadingButton
           type="submit"
           value="Login"
-          disabled={!valid || isPageLoading}
+          disabled={!isValid(password, passwordAgain) || isPageLoading}
           fullWidth
           variant="contained"
           color="primary"
+          loading={isPageLoading}
         >
           Create Account
-        </Button>
+        </LoadingButton>
       </form>
       <Dialog
         open={showModal}
-        onClose={modalDisagreeClick}
+        onClose={() => setShowModal(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
@@ -160,13 +148,18 @@ const CreateAccount = () => {
         <DialogActions sx={{ p: 1 }}>
           <Button
             color="error"
-            onClick={modalDisagreeClick}
+            onClick={() => {
+              setShowModal(false);
+            }}
           >
             Disagree
           </Button>
           <Button
             variant='contained'
-            onClick={modalAgreeClick}
+            onClick={() => {
+              setShowModal(false);
+              createAccount(password);
+            }}
             autoFocus
           >
             Agree
---
+++ frontend/src/Components/shared/PasswordValidation.tsx
@@ -0,0 +1,37 @@
+
+
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ErrorIcon from '@mui/icons-material/Error';
+import { Box, Typography } from "@mui/material";
+import { FunctionComponent } from "react";
+
+const ValidationMessage: FunctionComponent<{ label: string; validated: boolean; }> = ({ label, validated }) => {
+  return <Box display="flex" alignItems="center">
+    {validated ? <CheckCircleIcon color="primary" fontSize="small" sx={{ m: 0.5 }} /> : <ErrorIcon color="error" fontSize="small" sx={{ m: 0.5 }} />}
+    <Typography variant="subtitle2" color={validated ? "primary" : "error"}>{label}</Typography>
+  </Box>;
+};
+
+const hasValidLength = (password: string) => password.length >= 8 && password.length <= 30;
+const hasNumber = (password: string) => /\d/.test(password);
+const hasLowercase = (password: string) => /[a-z]/.test(password);
+const hasUppercase = (password: string) => /[A-Z]/.test(password);
+const hasSpecial = (password: string) => /\W/.test(password);
+const isMatch = (password1: string) => (password2: string) => password1 === password2;
+
+export const isValid = (password: string, passwordAgain: string) =>
+  [hasValidLength, hasNumber, hasLowercase, hasUppercase, hasSpecial, isMatch(passwordAgain)]
+    .map(check => check(password)).every(result => result);
+
+const PasswordValidation: FunctionComponent<{ password: string; passwordAgain: string }> = ({ password, passwordAgain }) => {
+  return <Box padding={1} sx={{ backgroundColor: "rgba(0, 0, 0, 0.05)", my: 2 }}>
+    <ValidationMessage validated={hasValidLength(password)} label="Contains 8 - 30 characters" />
+    <ValidationMessage validated={hasNumber(password)} label="Contains at least one number" />
+    <ValidationMessage validated={hasLowercase(password)} label="Contains at least one lowercase letter" />
+    <ValidationMessage validated={hasUppercase(password)} label="Contains at least one uppercase letter" />
+    <ValidationMessage validated={hasSpecial(password)} label="Contains at least one special character" />
+    <ValidationMessage validated={isMatch(password)(passwordAgain)} label="Passwords match" />
+  </Box>
+};
+
+export default PasswordValidation;
