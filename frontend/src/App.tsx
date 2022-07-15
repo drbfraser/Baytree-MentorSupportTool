e358849c64da5ee19447493588d1a07071229729
Viet Hung Nguyen <vhn@sfu.ca>
2022-07-15 09:54:50 -0700

    Split reset password route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import CreateAccount from "./Components/forms/CreateAccount";
 import Dashboard from "./Components/dashboard/Dashboard";
+import CreateAccount from "./Components/forms/CreateAccount";
+import Login from "./Components/forms/Login";
+import ResetPasswordRequest from "./Components/forms/ResetPassword";
+import ResetPasswordEmail from "./Components/forms/ResetPasswordRequest";
 import Goals from "./Components/goals/Goals";
 import Home from "./Components/home/Home";
-import Login from "./Components/forms/Login";
 import Notification from "./Components/Notification";
 import Profile from "./Components/Profile";
 import Questionnaire from "./Components/questionnaire/Questionnaire";
 import Records from "./Components/records/Records";
-import ResetPassword from "./Components/forms/ResetPassword";
 import Sessions from "./Components/sessions/Sessions";
 import PublicLayout from "./Components/shared/PublicLayout";
 import PrivateRoute from "./Utils/PrivateRoute";
@@ -19,13 +20,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* TODO: Split /resetPassword into 2 route
-          - /resetPassword/ -> Enter the email
-          - /resetPassword/{id} -> Reset Password
-        */}
-        <Route path="/resetPassword" element={<ResetPassword />} />
         <Route element={<PublicLayout />}>
-          <Route path="/createAccount" element={<CreateAccount />} />
+          <Route path="/resetPassword">
+            <Route path="" element={<ResetPasswordEmail />} />
+            <Route path=":resetPasswordId" element={<ResetPasswordRequest />} />
+          </Route>
+          <Route path="/createAccount/:accountCreationId" element={<CreateAccount />} />
           <Route element={<PublicRoute />}>
             <Route path="/login" element={<Login />} />
           </Route>
---
+++ frontend/src/Components/forms/ResetPasswordRequest.tsx
@@ -0,0 +1,63 @@
+import { LoadingButton } from "@mui/lab";
+import { Alert, AlertTitle, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { sendPasswordResetEmail } from "../../api/mentorAccount";
+
+const ResetPasswordRequest = () => {
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const requestResetPasswordEmail = () => {
+    setLoading(true);
+    sendPasswordResetEmail(email)
+      .then(() => setSuccess(true))
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  };
+
+  if (success) {
+    return <Alert severity="success">
+      An email with passwor reset has been sent to that specific address.
+    </Alert>
+  }
+
+  return <form
+    style={{width: "100%"}}
+    onSubmit={(e) => {
+    e.preventDefault();
+    requestResetPasswordEmail();
+  }}>
+    <Typography variant="h6">Enter your email</Typography>
+    {error && <Alert severity="error" sx={{my: 2}}>
+      <AlertTitle>Failed to send password reset email</AlertTitle>
+      Please try again or contact the administrator for further assistance
+    </Alert>}
+    <TextField
+      margin="normal"
+      required
+      fullWidth
+      name="email"
+      label="Email"
+      autoComplete="email"
+      value={email}
+      autoFocus
+      onChange={(e) => setEmail(e.target.value)}
+    />
+    <LoadingButton
+      type="submit"
+      value="Login"
+      disabled={!email || loading}
+      fullWidth
+      variant="contained"
+      color="primary"
+      loading={loading}
+      sx={{ mt: 3 }}
+    >
+      Send reset password email
+    </LoadingButton>
+  </form>
+};
+
+export default ResetPasswordRequest;
