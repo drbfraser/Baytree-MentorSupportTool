5db962209d5058ae01bb85dbc75a3f99e04665c5
NoelRiopel <nriopel@sfu.ca>
2022-07-18 16:57:47 -0700

    Implement Notes modal and clean up UI a bit

--- admin-frontend/components/pages/home/MentorSessionTrackingCard/MentorSessionsNotesModal.tsx
+++ admin-frontend/components/pages/home/MentorSessionTrackingCard/MentorSessionsNotesModal.tsx
@@ -1,6 +1,5 @@
-import { Skeleton, Typography } from "@mui/material";
-import { FunctionComponent, useEffect } from "react";
-import { toast } from "react-toastify";
+import { Skeleton, Stack, Typography } from "@mui/material";
+import { FunctionComponent } from "react";
 import { Session } from "../../../../api/backend/views/sessions";
 import useSessionDetails from "../../../../hooks/useSessionDetails";
 
@@ -15,19 +14,6 @@ const MentorSessionsNotesModal: React.FunctionComponent<
     parseInt(props.session.viewsSessionId)
   );
 
-  useEffect(() => {
-    if (sessionDetails === undefined) {
-      // skip initial useEffect on first component render
-      return;
-    } else if (sessionDetails === null) {
-      // an error occurred when getting the session details
-      toast.error(
-        "An error occurred when loading the session notes. Please try refreshing the page or contacting technical support."
-      );
-    } else {
-    }
-  }, [sessionDetails]);
-
   return sessionDetails === undefined ? (
     <Loading />
   ) : sessionDetails === null ? (
@@ -37,24 +23,121 @@ const MentorSessionsNotesModal: React.FunctionComponent<
     </Typography>
   ) : (
     <>
-      <Typography>Mentee Name: {sessionDetails.mentee?.name}</Typography>
-      <Typography>Notes:</Typography>
-      <br />
-      <Typography variant="body1">{sessionDetails.note}</Typography>
+      {/* Date */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Date:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.startDate}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Start Time */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Start Time:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.startTime}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Duration */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Duration:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.duration}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Venue Name */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Venue Name:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.venueName}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Session Group */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Session Group:{" "}
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">
+            {sessionDetails.sessionGroup.name}
+          </Typography>
+        </Stack>
+      </Stack>
+
+      {/* Activity */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Activity:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.activity}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Mentor Name */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Mentor Name:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">{sessionDetails.mentor.name}</Typography>
+        </Stack>
+      </Stack>
+
+      {/* Mentee Name */}
+      <Stack flexDirection="row">
+        <Typography variant="h6" margin="1rem" minWidth="9rem">
+          Mentee Name:
+        </Typography>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography variant="body1">
+            {sessionDetails.mentee
+              ? sessionDetails.mentee.name
+              : "Not Available"}
+          </Typography>
+        </Stack>
+      </Stack>
+
+      {/* Notes */}
+      <Typography
+        variant="h5"
+        marginTop="1rem"
+        marginLeft="1rem"
+        marginBottom="1rem"
+      >
+        Notes:
+      </Typography>
+      <Typography variant="body1" marginLeft="1rem">
+        {sessionDetails.note}
+      </Typography>
     </>
   );
 };
 
 const Loading: FunctionComponent = () => {
   return (
-    <>
+    <Stack margin="1rem">
       <Skeleton />
       <Skeleton />
       <Skeleton />
       <Skeleton />
       <Skeleton />
       <Skeleton />
-    </>
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+    </Stack>
   );
 };
 
