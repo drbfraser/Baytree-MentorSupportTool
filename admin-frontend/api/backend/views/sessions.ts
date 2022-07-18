daddcf4f62678e271bc715c20a4755004c884b12
NoelRiopel <nriopel@sfu.ca>
2022-07-18 15:53:47 -0700

    Create session details service function

--- admin-frontend/api/backend/views/sessions.ts
+++ admin-frontend/api/backend/views/sessions.ts
@@ -94,3 +94,73 @@ export const getSessionsFromViews = async (
     return null;
   }
 };
+
+export interface SessionDetails {
+  viewsSessionId: string;
+  viewsSessionGroupId: string;
+  name: string;
+  startDate: string;
+  startTime: string;
+  duration: string;
+  cancelled: string;
+  activity: string;
+  leadStaff: string;
+  venueId: string;
+  created: string;
+  updated: string;
+  venueName: string;
+  sessionGroup: SessionGroup;
+  mentor: any | null;
+  mentee: Mentee | null;
+  note: string | null;
+}
+
+interface SessionGroup {
+  viewsSessionGroupId: string;
+  name: string;
+  description: string;
+  leadStaff: string;
+  otherStaff: string;
+}
+
+interface Mentee {
+  menteeId: string;
+  name: string;
+}
+
+/** EXAMPLE RESPONSE FROM getSessionDetails()
+ * {
+    "viewsSessionId": "1",
+    "viewsSessionGroupId": "4",
+    "name": "'YS: Youth Mentoring 2021/2022' session",
+    "startDate": "2021-09-22",
+    "startTime": "17:00",
+    "duration": "01:00",
+    "cancelled": "0",
+    "activity": "Youth mentoring",
+    "leadStaff": "8",
+    "venueId": "1",
+    "created": "2021-09-24 12:27:06",
+    "updated": "2021-09-24 12:27:06",
+    "venueName": "Some Venue",
+    "sessionGroup": {
+        "viewsSessionGroupId": "4",
+        "name": "YS: Youth Mentoring 2021/2022",
+        "description": "Youth mentoring sessions run during the academic year 2021/2022. The sessions are run remotely or face-to-face for an hour.",
+        "leadStaff": "8",
+        "otherStaff": "6|1|43|74|48|76|79"
+    },
+    "mentor": null,
+    "mentee": {
+        "menteeId": "4",
+        "name": "Maria Ramirez"
+    },
+    "note": null
+}
+ */
+
+export const getSessionDetails = async (viewsSessionId: number) => {
+  const sessionDetailsEndpoint = `records/${viewsSessionId}/`;
+  const response = await backendGet<SessionDetails>(sessionDetailsEndpoint);
+  return response;
+};
