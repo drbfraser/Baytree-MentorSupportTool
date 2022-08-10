85c2213e9c30bc1b1ea45b5f3687b7ddc27123f1
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-10 03:10:01 -0700

    Disabled video recording

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
       bundler: "vite",
     },
   },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000"
   },
+  video: false,
 });
