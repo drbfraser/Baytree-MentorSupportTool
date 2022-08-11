0dfe6ea22364cb2cef713f86a7079c3548da308f
NoelRiopel <nriopel@sfu.ca>
2022-08-10 20:16:24 -0700

    Add session groups api methods to admin frontend

--- admin-frontend/api/backend/views/sessionGroups.ts
+++ admin-frontend/api/backend/views/sessionGroups.ts
@@ -1,4 +1,4 @@
-import { generateBackendGetFunc } from "../base";
+import { backendGet } from "../base";
 import { API_BASE_URL } from "../url";
 
 export interface SessionGroupResponse {
@@ -9,6 +9,29 @@ export interface SessionGroupResponse {
   otherStaff: string; // ex. 13|2|3
 }
 export const sessionGroupsFromViewsBackendEndpoint = `${API_BASE_URL}/views-api/session-groups`;
-export const getSessionGroupsFromViews = generateBackendGetFunc<SessionGroupResponse>(
-    sessionGroupsFromViewsBackendEndpoint
-);
+
+export const getSessionGroupsFromViews = async (
+  params:
+    | {
+        limit: number;
+        offset: number;
+        name: string;
+      }
+    | undefined
+) => {
+  const response = await backendGet<{
+    total: number;
+    data: SessionGroupResponse[];
+  }>(sessionGroupsFromViewsBackendEndpoint, params);
+
+  return response;
+};
+
+export const getQuestionnaireFromViews = async (id: number) => {
+  const response = await backendGet<SessionGroupResponse>(
+    sessionGroupsFromViewsBackendEndpoint,
+    { id }
+  );
+
+  return response;
+};
