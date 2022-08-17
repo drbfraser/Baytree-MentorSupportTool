03c7b7c0c501dc45ef45952a0ae90a1bd19cc5f4
Catherine Riopel <criopel@sfu.ca>
2022-08-16 21:46:16 -0700

    Create hook to use UkHoldays as well as method to map UkHoliday to EventInput

---
+++ frontend/src/hooks/useUkHolidays.ts
@@ -0,0 +1,33 @@
+import { EventInput } from "@fullcalendar/react";
+import { useEffect, useState } from "react";
+import { fetchUkHolidays, UkHoliday } from "../api/misc";
+import { EVENT_TYPE } from "./useSessionEvents";
+
+export const ukHolidayToCalendarEvent = (ukHoliday: UkHoliday) => {
+  return {
+    id: `${EVENT_TYPE.HOLIDAY}-${ukHoliday.id}`,
+    title: ukHoliday.name,
+    start: ukHoliday.date,
+    end: ukHoliday.date,
+    allDay: true,
+    rrule: undefined
+  } as EventInput;
+};
+
+const useUkHolidays = () => {
+  const [loading, setLoading] = useState(true);
+  const [events, setEvents] = useState([] as UkHoliday[]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchUkHolidays()
+      .then(({ data, error }) => {
+        if (error) setError(error);
+        else setEvents(data)
+      }).finally(() => setLoading(false));
+  }, []);
+
+  return { loadingUkHolidays: loading, ukHolidays: events, ukHolidaysError: error }
+};
+
+export default useUkHolidays;
