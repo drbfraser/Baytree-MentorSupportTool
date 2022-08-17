ac30af446b6337b5d46d684a4539527aa9b24df7
Catherine Riopel <criopel@sfu.ca>
2022-08-16 21:42:44 -0700

    Create serializer for uk holiday model

--- baytree_app/calendar_events/serializers.py
+++ baytree_app/calendar_events/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from .models import CalendarEvent
+from .models import CalendarEvent, UkHoliday
+
+class UkHolidaySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = UkHoliday
+    fields = [
+      "name",
+      "date"
+    ]
 
 class CalendarEventSerializer(serializers.ModelSerializer):
   class Meta:
