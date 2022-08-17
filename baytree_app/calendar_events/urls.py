dc77c42bdc0f34a8564f349cc582739b37328eb6
Catherine Riopel <criopel@sfu.ca>
2022-08-16 21:43:09 -0700

    Add url pattern for ulk holiday list api view

--- baytree_app/calendar_events/urls.py
+++ baytree_app/calendar_events/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
   CalendarEventListAPIView,
+  CalendarEventUkHolidayListAPIView,
   CalendarEventCreateAPIView,
   CalendarEventUpdateDestroyAPIVIew
 )
@@ -9,6 +10,7 @@ from .views import (
 # /api/calendar_events/
 urlpatterns = [
   path('', CalendarEventListAPIView.as_view()),
+  path('uk_holidays/', CalendarEventUkHolidayListAPIView.as_view()),
   path('create/', CalendarEventCreateAPIView.as_view()),
   path('<int:pk>/', CalendarEventUpdateDestroyAPIVIew.as_view()),
 ]
