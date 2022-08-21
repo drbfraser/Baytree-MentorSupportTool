70b16a2f84996d9321d198d4e04137918b93a27a
Catherine Riopel <criopel@sfu.ca>
2022-08-21 15:20:22 -0700

    Reset to vite config from commit #dd92fcc9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,10 +21,13 @@ export default defineConfig({
   build: {
     sourcemap: false,
     outDir: "./build",
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  plugins: [react()]
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules'))
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      }
+    }
+  }
 })
