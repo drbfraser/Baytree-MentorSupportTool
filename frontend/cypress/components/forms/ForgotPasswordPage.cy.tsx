38184b19029b2aece3c599d15f21a144e9d954a9
Karan Sachdeva <karan_sachdeva@sfu.ca>
2022-11-04 00:18:04 -0700

    added component tests for login, resetPassword and forgotPassword pages

---
+++ frontend/cypress/components/forms/ForgotPasswordPage.cy.tsx
@@ -0,0 +1,20 @@
+import ResetPasswordRequest from "@pages/ResetPasswordRequest";
+
+describe('Forgot Password Component Test', () => {
+  beforeEach(() => {
+    cy.mountWithRouter(<ResetPasswordRequest />)
+  })
+
+  it('should render correct title', () => {
+    cy.contains('Enter your email', {matchCase: true})
+  })
+
+  it('should render disabled button', () => {
+    cy.get('button').should('be.disabled')
+  })
+
+  it('should render enabled button after typing', () => {
+    cy.get('input[name="email"]').type("abc@example.com")
+    cy.get('button').should('be.enabled')
+  })
+})
