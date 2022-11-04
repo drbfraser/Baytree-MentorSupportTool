7368649bd8b68055f1d60cebee6e0ce8d11be5f3
Andrew L <ala215@sfu.ca>
2022-11-04 08:52:33 +0000

    Revert "Merge branch 'component-tests' into 'master'"
    This reverts merge request !113

--- frontend/cypress/components/forms/Login.cy.tsx
+++ frontend/cypress/components/forms/Login.cy.tsx
@@ -1,20 +1,16 @@
 import Login from "@pages/Login";
 
-describe('Login Page Component Test', () => {
+describe('Login page', () => {
   beforeEach(() => {
     cy.mountWithRouter(<Login />)
   })
 
-  it('should render login title', () => {
-    cy.contains('Log in to Mentor Portal', {matchCase: true})
+  it('should render title', () => {
+    cy.contains('log in to mentor portal', {matchCase: false})
   })
 
   it('should render disabled button', () => {
     cy.get('button').should('be.disabled')
-  })
-  
-  it('should render forgot password button', () => {
-    cy.contains("a", "Forgot Password?").should("have.attr", "href", "/ResetPassword");
   })
 
   it('should enable button after typing', () => {
