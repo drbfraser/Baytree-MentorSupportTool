04f7729ff5f455ab3e0fe2adebe356427174ec32
Andrew <andrewliang0123@gmail.com>
2022-12-18 00:29:02 -0800

    fix stg logging

--- baytree_app/baytree_app/views.py
+++ baytree_app/baytree_app/views.py
@@ -160,14 +160,17 @@ class CookieTokenObtainPairView(TokenObtainPairView):
         # If wrong password, (unauthorized), don't return additional info or cookies
 
         Flogging = FluentLoggingHandler()
-        replace_number = 4
-        replace_str = "-"*replace_number + request.data["email"][replace_number:]
-        Flogging.sendLog("login request by " + str(replace_str))
-        Flogging.sendInfoLog("login request by INFO: " + str(replace_str))
+        if request.data["email"]:
+
+            replace_number = 4
+            replace_str = "-"*replace_number + request.data["email"][replace_number:]
+            Flogging.sendInfoLog("login request by: " + str(replace_str))
+
+
 
         if response.status_code == 401:
             # login-failures logging
-            Flogging.sendLog(str(response.data["detail"]))
+            Flogging.sendInfoLog(str(response.data["detail"]))
             return super().finalize_response(request, response, *args, **kwargs)
 
         if response.data.get("refresh"):
@@ -214,7 +217,7 @@ class CookieTokenObtainPairView(TokenObtainPairView):
         response.data["is_superuser"] = user.is_superuser
 
         # login success logging
-        Flogging.sendLog("login as admin") if user.is_superuser else Flogging.sendLog("login as mentor")
+        Flogging.sendInfoLog("login as admin") if user.is_superuser else Flogging.sendInfoLog("login as mentor")
 
         response.data["last_login"] = user.last_login
         user.last_login = make_aware(datetime.now())
