e669f99fc46cf9fbe4f409adef8fccbbae2002ac
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-01-28 23:55:43 -0800

    Added test code for reviewer to test

--- baytree_app/resources/views.py
+++ baytree_app/resources/views.py
@@ -1,10 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+# from baytree_app.FluentLoggingHandler import FluentLoggingHandler
 
 base_url = 'https://thebaytreecentre.sharepoint.com/:f:/g/Ej7DxK0KjzNBuTwQ_lU-0bMBdMNHOWzHi2bzGJB86G4Bjg'
+# logger = FluentLoggingHandler()
 
 @api_view(("GET",))
 def get_resource(request):   
+    # logger.writeRequestReceivedLog(request)
+    # logger.writeMessageLog("Test log", logger.INFO)
+    # response = Response(base_url)
+    # logger.writeResponseSentLog(response)
     return Response(base_url)
 
 
