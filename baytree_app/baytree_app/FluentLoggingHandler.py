503ab8e0f6a7d510ad87fb7cccd536ccc1022296
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-01-28 23:51:48 -0800

    Removed commented code

--- baytree_app/baytree_app/FluentLoggingHandler.py
+++ baytree_app/baytree_app/FluentLoggingHandler.py
@@ -13,27 +13,9 @@ class FluentLoggingHandler:
 
     def __init__(self):
         pass
-        # if os.environ.get("LOGGING_URL"):
-        #     self.host = os.environ.get("LOGGING_URL")
-        # else:
-        #     self.host = "http://fluent-bit:24223/"
-        # self.info_logger = logging.getLogger("django_message")
-
-    # def sendLog(self, message):
-    #     try:
-    #         obj = {"log: ": message}
-    #         requests.post(""+self.host, json=obj)
-    #     except Exception as e:
-    #         print(e)
 
     def sendInfoLog(self, message):
         pass
-        # try:
-        #     self.info_logger.setLevel(logging.INFO)
-        #     #obj = {"log: ": message}
-        #     self.info_logger.info(message)
-        # except Exception as e:
-        #     print(e)
 
     def writeRequestReceivedLog(self, request, message = {}):
         try:
