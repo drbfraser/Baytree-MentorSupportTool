6cc49a8ec02ceceedd88a6b70ee1082ae486684c
chhokara <chhokara@gmail.com>
2023-02-08 15:43:11 -0800

    update the test plan for testing out logs

--- baytree_app/resources/views.py
+++ baytree_app/resources/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # from baytree_app.FluentLoggingHandler import FluentLoggingHandler
-
+# import os
+# import traceback
 
 base_url = 'https://thebaytreecentre.sharepoint.com/:f:/g/Ej7DxK0KjzNBuTwQ_lU-0bMBdMNHOWzHi2bzGJB86G4Bjg'
 # logger = FluentLoggingHandler()
@@ -9,15 +10,19 @@ base_url = 'https://thebaytreecentre.sharepoint.com/:f:/g/Ej7DxK0KjzNBuTwQ_lU-0b
 
 @api_view(("GET",))
 def get_resource(request):
-    # loggingMessage = {
-    #     "content": "Test log",
-    #     "data": None # This can be actual data, or a string, a link, or an exception (e.__str()__)
-    # }
-    # logger.debug(loggingMessage)
-    # logger.info(loggingMessage)
-    # logger.warning(loggingMessage)
-    # logger.error(loggingMessage)
-    # logger.critical(loggingMessage)
+    # test_var = "testing logging methods"
+    # file_path = os.getcwd()
+
+    # logger.debug("test_var", test_var, file_path,
+    #              13, "logging test_var's value")
+    # logger.info("N/A", None, "Requesting a resource")
+    # logger.warning(file_path, 19, "Logging a warning")
+    # try:
+    #     raise ValueError("Error")
+    # except ValueError as e:
+    #     stack_trace = traceback.format_exc()
+    #     logger.error(file_path, 22, stack_trace, "Error has occured")
+    #     logger.critical(file_path, 22, stack_trace,
+    #                     "Critical error has occured")
+
     return Response(base_url)
-
-
