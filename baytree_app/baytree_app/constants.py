2326feb27cd479943c16a0822f69dae13f313337
KatelynKim <katelynk@sfu.ca>
2023-02-15 00:08:18 -0800

    Set default base url

--- baytree_app/baytree_app/constants.py
+++ baytree_app/baytree_app/constants.py
@@ -1,5 +1,5 @@
 import os
 
-VIEWS_BASE_URL = os.environ["VIEWS_BASE_URL"]
+VIEWS_BASE_URL = os.environ["VIEWS_BASE_URL"] or "http://views-mock:5001/"
 VIEWS_USERNAME = os.environ["VIEWS_USERNAME"]
 VIEWS_PASSWORD = os.environ["VIEWS_PASSWORD"]
