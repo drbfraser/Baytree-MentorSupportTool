375a77e20644222ce7d6abbf79522dd9d0f992ec
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-02-21 15:57:03 -0800

    Added endpoint with mock data and configured url

--- views_mock/views_api/urls.py
+++ views_mock/views_api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin/valuelists/sessiongroup/volunteeringtypes', views.get_volunteering_types_endpoint, name='volunteer_types'),
+    path('evidence/questionnaires/<int:questionnaireId>', views.get_questionnaire_by_id_endpoint, name='questionnaires'),
+    path('evidence/questionnaires/search', views.search_questionnaires_endpoint, name='search_questionnaires'),
 ]
