bddcbe060118caf638ae82005a5509433f7310c6
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-02-21 17:38:04 -0800

    Created mock endpoints and data

--- views_mock/views_api/urls.py
+++ views_mock/views_api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin/valuelists/sessiongroup/volunteeringtypes', views.get_volunteering_types_endpoint, name='volunteer_types'),
+    path('work/sessiongroups/<int:sessionGroupId>', views.get_session_group_by_id_endpoint, name='session_group'),
+    path('work/sessiongroups/search', views.search_session_groups_endpoint, name='search_session_groups'),
 ]
