3848bd385d27d537e7bbd4dfb2eb222e7efbd46f
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-02-22 17:58:59 -0800

    Added mock endpoint and data

--- views_mock/views_api/views.py
+++ views_mock/views_api/views.py
@@ -17,3 +17,28 @@ def get_volunteering_types_endpoint(request):
         }
     }
   return Response(data, 200)
+
+@api_view(("GET",))
+def get_staff_associations(request, staffId):
+  # Note that if the staff has a single association, the object is not wrapped in a list
+  # Otherwise, associations is a list
+  data = {
+    "associations": {
+      "3": {
+        "AssociationID": "3",
+        "MasterType": "Person",
+        "MasterID": "5",
+        "SlaveType": "Staff",
+        "SlaveID": str(staffId),
+        "Association": "Mentee",
+        "Description": "",
+        "StartDate": "2021-06-15",
+        "EndDate": "0000-00-00",
+        "Created": "2021-09-24 12:47:06",
+        "CreatedBy": "group.customer",
+        "Updated": "2021-11-05 01:07:27",
+        "UpdatedBy": "group.earth"
+      }
+    }
+  }
+  return Response(data, 200)
