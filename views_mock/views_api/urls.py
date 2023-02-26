7a022a32c56ff200f439c94d8b2d3ff4be7645a8
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-02-25 23:30:48 -0800

    Added mock data and set up endpoint

--- views_mock/views_api/urls.py
+++ views_mock/views_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/valuelists/sessiongroup/volunteeringtypes', views.get_volunteering_types_endpoint, name='volunteer_types'),
     path('contacts/staff/<int:staffId>/associations', views.get_staff_associations, name='staff_associations'),
     path('contacts/participants/search', views.search_participants_endpoint, name='search_participants'),
+    path('contacts/participants/<int:menteeId>', views.search_participant_by_id_endpoint, name='search_participant_by_id'),
 ]
