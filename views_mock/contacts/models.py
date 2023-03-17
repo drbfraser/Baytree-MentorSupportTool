2fdf4d24e72b894fad97968bb3a0ab979b90f7ec
KatelynKim <katelynk@sfu.ca>
2023-03-16 22:43:30 -0700

    Set Ethnicity and FirstLanguage as required fields

--- views_mock/contacts/models.py
+++ views_mock/contacts/models.py
@@ -16,18 +16,18 @@ class Person(models.Model):
     Forename = models.CharField(max_length=30)
     Surname = models.CharField(max_length=30)
     Email = models.EmailField(null=True)
-    DateOfBirth = models.DateField()
+    DateOfBirth = models.DateField(null=False)
     Country = models.CharField(max_length=50, null=True)
 
 class Participant(models.Model):
     PersonID = models.OneToOneField(Person, primary_key=True, on_delete=models.CASCADE, related_name="participant", db_column="PersonID")
-    FirstLanguage_P_88 = models.CharField(max_length=20, null=True)
-    Ethnicity = models.CharField(max_length=20, null=True)
+    FirstLanguage_P_88 = models.CharField(max_length=20)
+    Ethnicity = models.CharField(max_length=20)
 
 class Volunteers(models.Model):
     PersonID = models.OneToOneField(Person, primary_key=True, on_delete=models.CASCADE, related_name="volunteer", db_column="PersonID")
-    Whatisyourfirstlanguage_V_19 = models.CharField(max_length=20, null=True)
-    Ethnicity_V_15 = models.CharField(max_length=20, null=True)
+    Whatisyourfirstlanguage_V_19 = models.CharField(max_length=20)
+    Ethnicity_V_15 = models.CharField(max_length=20)
 
 
 
