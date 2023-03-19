bc2acb825f4f88b4fc38d4eb9e3cc115df773ea9
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-03-18 23:26:59 -0700

    Addressed comments

--- views_mock/evidence/models.py
+++ views_mock/evidence/models.py
@@ -1,29 +1,29 @@
 from django.db import models
 
 class Questionnaire(models.Model):
-    # ID field is automatically generated
-    title = models.CharField(verbose_name="Title", max_length=100)
-    description = models.TextField(verbose_name="Description")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Updated")
-    createdBy = models.CharField(verbose_name="CreatedBy", max_length=50)
-    updatedBy = models.CharField(verbose_name="UpdatedBy", max_length=50)
+    QuestionnaireID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=100)
+    Description = models.TextField()
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+    CreatedBy = models.CharField(max_length=50)
+    UpdatedBy = models.CharField(max_length=50)
 
 class Question(models.Model):
-    # question id field is automatically generated
-    questionID = models.ForeignKey(Questionnaire, on_delete=models.CASCADE, verbose_name="QuestionnaireID")
-    sourceID = models.IntegerField(verbose_name="SourceID")
-    question = models.CharField(verbose_name="Question", max_length=500)
-    valueListID = models.IntegerField(verbose_name="valueListID")
-    inputType = models.CharField(verbose_name="inputType", max_length=50)
-    validation = models.CharField(verbose_name="validation", max_length=100)
-    category = models.CharField(verbose_name="category", max_length=50)
-    enabled = models.IntegerField(verbose_name="enabled")
+    QuestionID = models.AutoField(primary_key=True)
+    QuestionnaireID = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    SourceID = models.IntegerField()
+    Question = models.CharField(max_length=500)
+    valueListID = models.IntegerField()
+    inputType = models.CharField(max_length=50)
+    validation = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    enabled = models.IntegerField()
 
 class AnswerSet(models.Model):
-    questionID = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="QuestionID", related_name="questionIdOfQuestion")
-    answerID = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="AnswerID", related_name="answerIdOfQuestion")
-    answer = models.CharField(verbose_name="Answer", max_length=1000)
-    entityType = models.CharField(verbose_name="EntityType", max_length=100)
-    entityID = models.IntegerField(verbose_name="EntityID")
-    date = models.DateTimeField(verbose_name="Date")
+    QuestionID = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="questionIdOfQuestion")
+    AnswerID = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answerIdOfQuestion")
+    Answer = models.CharField(max_length=1000)
+    EntityType = models.CharField(max_length=100)
+    EntityID = models.IntegerField()
+    Date = models.DateTimeField()
