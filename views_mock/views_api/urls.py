33f3de4dde35df4b667ff2547f4058ca142e4e76
KatelynKim <katelynk@sfu.ca>
2023-03-19 02:01:33 -0700

    Remove mock participants endpoints

--- views_mock/views_api/urls.py
+++ views_mock/views_api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('contacts/staff/<int:staffId>/associations', views.get_staff_associations, name='staff_associations'),
     path('contacts/participants/search', search_participants, name='search_participants'),
     path('work/sessiongroups/<int:sessionGroupId>', views.get_session_group_by_id_endpoint, name='session_group'),
-    path('contacts/participants/<int:menteeId>', views.search_participant_by_id_endpoint, name='search_participant_by_id'),
     path('work/sessiongroups/search', views.search_session_groups_endpoint, name='search_session_groups'),
     path('evidence/questionnaires/<int:questionnaireId>', views.get_questionnaire_by_id_endpoint, name='questionnaires'),
     path('evidence/questionnaires/search', views.search_questionnaires_endpoint, name='search_questionnaires'),
