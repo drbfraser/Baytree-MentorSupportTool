3161cf1e5b17d5110fb8cba1b786153b5101d2b4
chhokara <chhokara@gmail.com>
2023-03-19 12:41:59 -0700

    create models

--- views_mock/admin_valuelists/models.py
+++ views_mock/admin_valuelists/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ValueList(models.Model):
+    ValueListID = models.IntegerField(primary_key=True)
+    Type = models.CharField(max_length=50)
+    Name = models.CharField(max_length=50)
+
+
+class ValueListItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    valueList = models.OneToOneField(
+        ValueList, on_delete=models.CASCADE, related_name="ValueListItem", db_column="ValueListID")
+    value = models.CharField(max_length=50)
