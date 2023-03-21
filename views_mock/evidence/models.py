f5766e99472743c93331a7d4be8fe6210190b2a9
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-03-20 22:30:53 -0700

    Made fields required as needed

--- views_mock/evidence/models.py
+++ views_mock/evidence/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 class Questionnaire(models.Model):
     QuestionnaireID = models.AutoField(primary_key=True)
-    Title = models.CharField(max_length=100)
+    Title = models.CharField(max_length=100, null=False)
     Description = models.TextField()
-    Created = models.DateTimeField(auto_now_add=True)
-    Updated = models.DateTimeField(auto_now=True)
-    CreatedBy = models.CharField(max_length=50)
-    UpdatedBy = models.CharField(max_length=50)
+    Created = models.DateTimeField(auto_now_add=True, null=False)
+    Updated = models.DateTimeField(auto_now=True, null=False)
+    CreatedBy = models.CharField(max_length=50, null=False)
+    UpdatedBy = models.CharField(max_length=50, null=False)
 
 class Question(models.Model):
     QuestionID = models.AutoField(primary_key=True)
     QuestionnaireID = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
     SourceID = models.IntegerField()
-    Question = models.CharField(max_length=500)
+    Question = models.CharField(max_length=500, null=False)
     valueListID = models.IntegerField()
-    inputType = models.CharField(max_length=50)
+    inputType = models.CharField(max_length=50, null=False)
     validation = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     enabled = models.IntegerField()
@@ -23,9 +23,9 @@ class Question(models.Model):
 class AnswerSet(models.Model):
     AnswerSetID = models.AutoField(primary_key=True)
     QuestionID = models.ForeignKey(Question, on_delete=models.CASCADE)
-    EntityType = models.CharField(max_length=100)
-    EntityID = models.IntegerField()
-    Date = models.DateTimeField(auto_now_add=True)
+    EntityType = models.CharField(max_length=100, null=False)
+    EntityID = models.IntegerField(null=False)
+    Date = models.DateTimeField(auto_now_add=True, null=False)
 
 class Answer(models.Model):
     AnswerID = models.AutoField(primary_key=True)
