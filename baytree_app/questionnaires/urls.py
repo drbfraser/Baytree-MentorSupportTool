ae08ada722dc7418169dcd4a7e36062a2319604c
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-03-26 00:00:39 -0700

    Deleted unnecessary files

--- baytree_app/questionnaires/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_questionnaire, submit_answer_set
-
-# /api/questionnaires/
-urlpatterns = [
-    path('questionnaire/', get_questionnaire),
-    path('questionnaire/submit/', submit_answer_set),
-]
