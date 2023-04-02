ac786664cb811fcb5fb3a4122303fbb78c862346
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-04-01 23:09:43 -0700

    Removed comments

--- views_mock/views_api/valuelists.py
+++ views_mock/views_api/valuelists.py
@@ -15,17 +15,6 @@ def get_valuelists_endpoint(request, valueListID):
 @api_view(("GET",))
 @permission_classes([AdminPermissions | MentorPermissions])
 def get_volunteering_types_endpoint(request):
-
-  # data = {
-  #       "items": {
-  #         "volunteer_types": [
-  #             {"id": 1, "type_name": "Community Service"},
-  #             {"id": 2, "type_name": "Environmental Work"},
-  #             {"id": 3, "type_name": "Animal Care"},
-  #         ]
-  #       }
-  #   }
-  # return Response(data, 200)
   data = {}
   items = {}
 
