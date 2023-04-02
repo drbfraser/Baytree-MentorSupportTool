b8a745054856790a712afe1c893121fbbd1e9cfd
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2023-04-01 23:09:02 -0700

    Moved functions and updated urls

--- views_mock/views_api/urls.py
+++ views_mock/views_api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from . import views
 from .contacts import search_participants
+from .valuelists import get_volunteering_types_endpoint, get_activities_endpoint, get_venues_endpoint, get_valuelists_endpoint
 
 urlpatterns = [
-    path('admin/valuelists/sessiongroup/volunteeringtypes', views.get_volunteering_types_endpoint, name='volunteer_types'),
+    path('admin/valuelists/sessiongroup/volunteeringtypes', get_volunteering_types_endpoint, name='volunteer_types'),
+    path('admin/valuelists/sessiongroup/agencyactivities', get_activities_endpoint, name='activities'),
+    path('admin/valuelists/sessiongroup/venues', get_venues_endpoint, name='venues'),
+    path('admin/valuelists/sessiongroup/<int:valueListID>', get_valuelists_endpoint, name='valuelists'),
+
     path('contacts/staff/<int:staffId>/associations', views.get_staff_associations, name='staff_associations'),
     path('contacts/participants/search', search_participants, name='search_participants'),
+
     path('work/sessiongroups/<int:sessionGroupId>', views.get_session_group_by_id_endpoint, name='session_group'),
     path('work/sessiongroups/search', views.search_session_groups_endpoint, name='search_session_groups'),
+    
     path('evidence/questionnaires/<int:questionnaireId>', views.get_questionnaire_by_id_endpoint, name='questionnaires'),
-    path('evidence/questionnaires/search', views.search_questionnaires_endpoint, name='search_questionnaires'),
-    path('admin/valuelists/sessiongroup/agencyactivities', views.get_activities_endpoint, name='activities'),
-    path('admin/valuelists/sessiongroup/venues', views.get_venues_endpoint, name='venues'),
+    path('evidence/questionnaires/search', views.search_questionnaires_endpoint, name='search_questionnaires')
 ]
