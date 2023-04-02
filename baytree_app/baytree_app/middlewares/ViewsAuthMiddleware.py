26cc1807358bb7527294b8b77d7af163f0247cc4
KatelynKim <katelynk@sfu.ca>
2023-04-02 12:54:38 -0700

    Refactor and add async support to ViewsAuthMiddleware

--- baytree_app/baytree_app/middlewares/ViewsAuthMiddleware.py
+++ baytree_app/baytree_app/middlewares/ViewsAuthMiddleware.py
@@ -3,33 +3,61 @@ from django.urls import resolve
 from django.http import HttpResponse
 import base64
 from baytree_app.FluentLoggingHandler import FluentLoggingHandler
+from django.utils.decorators import sync_and_async_middleware
+import asyncio
 
+@sync_and_async_middleware
 class ViewsAuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+        if asyncio.iscoroutinefunction(get_response):
+           async def middleware(request):
+              response = await get_response(request)
+              return response
+        else:
+           def middleware(request):
+              response = get_response(request)
+              return response
+        self.middleware = middleware
+
+    def process_view(self, request, view, *args, **kwargs):
+      if asyncio.iscoroutinefunction(view):
+         loop = asyncio.new_event_loop()
+         asyncio.set_event_loop(loop)
+         response = loop.run_until_complete(view(request))
+         return response
+
+      return view(request)
+
     def __call__(self, request):
         path = request.META["PATH_INFO"]
-        headers = {"Accept": "*/*"}
+        authorization_handlers = {
+           "http://views-mock:5001/": authorize_mock_views,
+           "https://app.viewsapp.net/api/restful/": authorize_views_app
+        }
 
-        # For views requests, determine the appropriate authentication method depending on the destination of the request (Views App or Mock Views)
+        # For views requests, run the
         if (path.startswith("/api/views-api")):
-          if os.environ["VIEWS_BASE_URL"]=="http://views-mock:5001/":
-            access_token = request.COOKIES.get("access_token")
-            if not access_token:
-              FluentLoggingHandler.error("Cannot authorize a request to mock Views due to missing access token!")
-              return HttpResponse('Access token is missing', status=401)
-            headers["Cookie"] = "access_token=" + access_token
+          authorization_handlers[os.environ["VIEWS_BASE_URL"]](request)
+          response = self.middleware(request)
 
-          elif os.environ["VIEWS_BASE_URL"]=="https://app.viewsapp.net/api/restful/":
-            auth=(os.environ["VIEWS_USERNAME"]+":" + os.environ["VIEWS_PASSWORD"]).encode("utf-8")
-            base64_auth=base64.b64encode(auth).decode("utf-8")
-            headers["Authorization"] = "Basic " + base64_auth
-
-          resolved_view = resolve(request.path_info)
-          response = resolved_view.func(request, headers)
-          response.render()
           return response
 
         else:
           return self.get_response(request)
+
+def authorize_mock_views(request):
+  access_token = request.COOKIES.get("access_token")
+  if (access_token):
+    request.META["VIEWS_AUTHORIZATION"] = "access_token=" + access_token
+  else:
+    FluentLoggingHandler.error("Cannot authorize a request to mock Views due to missing access token!")
+    return HttpResponse('Access token is missing', status=401)
+
+def authorize_views_app(request):
+  auth=(os.environ["VIEWS_USERNAME"]+":" + os.environ["VIEWS_PASSWORD"]).encode("utf-8")
+  base64_auth=base64.b64encode(auth).decode("utf-8")
+  request.META['VIEWS_AUTHORIZATION'] = "Basic " + base64_auth
+
+
