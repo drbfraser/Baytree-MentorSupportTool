c6d3fb6bab1d65e5a73f474e516af83dea0c2674
KatelynKim <katelynk@sfu.ca>
2023-04-11 19:04:53 -0700

    Add a volunteer view to views_mock

--- views_mock/views_api/contacts.py
+++ views_mock/views_api/contacts.py
@@ -1,7 +1,7 @@
 from users.permissions import MentorPermissions
 from users.permissions import AdminPermissions
 from rest_framework.decorators import permission_classes, api_view
-from contacts.models import Participant, Person
+from contacts.models import Participant, Person, Volunteer
 from rest_framework.response import Response
 
 from xml.etree.ElementTree import Element, SubElement, tostring
@@ -20,26 +20,39 @@ PARTICIPANT_FIELDS = [
     "Ethnicity"
 ]
 
+VOLUNTEER_FIELDS = [
+    "Whatisyourfirstlanguage_V_19",
+    "Ethnicity_V_15"
+]
+
+@api_view(("GET",))
+@permission_classes([AdminPermissions | MentorPermissions])
+def search_volunteers(request):
+    person_ids = request.GET.getlist('PersonID[]')
+    volunteer_objects = Volunteer.objects.filter(id__in=person_ids) if person_ids else Volunteer.objects.all()
+    xml_element = create_xml_element(volunteer_objects, "volunteers", VOLUNTEER_FIELDS)
+    return Response(data=tostring(xml_element), status=200)
+
 @api_view(("GET",))
 @permission_classes([AdminPermissions | MentorPermissions])
 def search_participants(request):
     person_ids = request.GET.getlist('PersonID[]')
     participant_objects = Participant.objects.filter(id__in=person_ids) if person_ids else Participant.objects.all()
-    xml_element = create_xml_element(participant_objects)
+    xml_element = create_xml_element(participant_objects, "participants", PARTICIPANT_FIELDS)
     return Response(data=tostring(xml_element), status=200)
 
-def create_xml_element(participant_objects):
+def create_xml_element(member_objects, member_type, member_fields):
     root = Element("contacts")
-    participants = SubElement(root, "participants", {"count": str(len(participant_objects))})
+    participants = SubElement(root, member_type, {"count": str(len(member_objects))})
 
-    for participant_object in participant_objects:
+    for member_object in member_objects:
         # Append PersonID element
-        participant = SubElement(participants, "participant", {"id": str(participant_object.person.PersonID)})
+        participant = SubElement(participants, member_type, {"id": str(member_object.person.PersonID)})
         person_id_element = SubElement(participant, "PersonID")
-        person_id_element.text = str(participant_object.person.PersonID)
+        person_id_element.text = str(member_object.person.PersonID)
 
         # Append the remaining fields to the participant element
-        person_object = Person.objects.get(PersonID=participant_object.person.PersonID)
+        person_object = Person.objects.get(PersonID=member_object.person.PersonID)
         participant = append_sub_elements(
             root=participant,
             object_fields=person_object.__dict__,
@@ -47,8 +60,8 @@ def create_xml_element(participant_objects):
           )
         participant = append_sub_elements(
             root=participant,
-            object_fields=participant_object.__dict__,
-            model_fields=PARTICIPANT_FIELDS
+            object_fields=member_object.__dict__,
+            model_fields=member_fields
           )
     return root
 
