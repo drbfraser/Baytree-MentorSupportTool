38b293c69a4d4da9c71f4723dfa6607469cff860
chhokara <chhokara@gmail.com>
2023-04-13 17:34:47 -0700

    create new CI-CD process for fluent logger tests

---
+++ baytree_app/baytree_app/testing/test_failed_log.py
@@ -0,0 +1,19 @@
+from baytree_app.FluentLoggingHandler import FluentLoggingHandler
+import logging
+
+# log file formatter
+formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s')
+
+# message logger setup
+FluentLoggingHandler.messageLogger.setLevel(logging.DEBUG)
+applicationFileHandler = logging.FileHandler(
+    "/code/server_logs/server_application.log")
+applicationFileHandler.setFormatter(formatter)
+FluentLoggingHandler.messageLogger.addHandler(applicationFileHandler)
+
+# sending critical log
+FluentLoggingHandler.critical("A fluent logger test has failed")
+
+# cleanup handler
+FluentLoggingHandler.messageLogger.removeHandler(applicationFileHandler)
