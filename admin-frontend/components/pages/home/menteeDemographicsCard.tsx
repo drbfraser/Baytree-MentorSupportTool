f4352a6e57509a58e03bf9ccba1ba505efcdf260
KatelynKim <katelynk@sfu.ca>
2023-04-17 15:24:26 -0700

    Refactored participants endpoint to be cleaner and asynchronous

--- admin-frontend/components/pages/home/menteeDemographicsCard.tsx
+++ admin-frontend/components/pages/home/menteeDemographicsCard.tsx
@@ -4,7 +4,7 @@ import { COLORS, HELP_MESSAGE } from '../../../constants/constants'
 
 import React, { useEffect, useState } from 'react'
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
-import { Button, ButtonGroup, Skeleton, Typography } from '@mui/material'
+import { Button, ButtonGroup, CircularProgress, Typography } from '@mui/material'
 import moment from 'moment'
 import { toast } from 'react-toastify'
 import {
@@ -121,20 +121,7 @@ const MenteeDemographicsCard: React.FC<{}> = () => {
   return (
     <StyledMenteeDemographicsCard>
       {loadingData ? (
-        <>
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
-        </>
+        <CircularProgress/>
       ) : (
         <>
           <Header
--- baytree_app/views_api/participants.py
+++ baytree_app/views_api/participants.py
@@ -4,9 +4,10 @@ from users.permissions import AdminPermissions
 from baytree_app.constants import VIEWS_BASE_URL
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.response import Response
-from rest_framework import status
 import requests
 import xmltodict
+import aiohttp
+from rest_framework.renderers import JSONRenderer
 
 participants_base_url = VIEWS_BASE_URL + "contacts/participants/"
 
@@ -36,11 +37,8 @@ WHAT IS A PARTICIPANT:
 For Baytree's use case of the Views API, Participants in their Views database are the same as Mentees.
 These participant records in Views contain contact and general information about the Mentee, .etc.
 """
-
-
-@api_view(("GET",))
 @permission_classes([AdminPermissions | MentorPermissions])
-def get_participants_endpoint(request):
+async def get_participants(request):
     """
     Handles a request from the client browser and calls get_participants
     to return its response to the client.
@@ -50,60 +48,35 @@ def get_participants_endpoint(request):
         "Accept": "application/xml"
     }
 
-    ids = request.GET.getlist("id")
-    ids = None if ids == [] else ids
+    id_list = request.GET.getlist("id", [])
+    searchEmail = request.GET.get("searchEmail", '')
+    searchFirstName = request.GET.get("searchFirstName", '')
+    searchLastName = request.GET.get("searchLastName", '')
+    offset = request.GET.get("offset", '')
+    limit = request.GET.get("limit", '')
 
-    if ids != None:
-        response = get_participants(ids, headers=headers)
-    else:
-        response = get_participants(
-            limit=request.GET.get("limit", None),
-            offset=request.GET.get("offset", None),
-            headers=headers
-        )
+    views_request_url = participants_base_url + '/search?'
 
-    return Response(response, status=status.HTTP_200_OK)
+    if id_list:
+        for id in id_list:
+            views_request_url += "&PersonID[]={}".format(id)
 
+    views_request_url += '&Email=' + searchEmail
+    views_request_url += '&Forename=' + searchFirstName
+    views_request_url += '&Surname=' + searchLastName
+    views_request_url += '&pageFold=' + str(limit)
+    views_request_url += '&offset=' + str(offset)
 
-def get_participants(ids=None, headers='', limit: int = 5, offset: int = 0):
-    """
-    Gets participants from Views API.
-    If an id argument is provided, the participant with a matching PersonId will be returned.
-    The limit and offset parameters are used to implement pagination.
-    The limit parameter determines how many participants to return from the Views API.
-    The offset parameter determines which participant to start at when asking for
-    a number of participants from Views when using the limit parameter.
-    So, if limit = 5 and offset = 5, this would say: "give me 5 participants,
-    but skip the first 5 in the total participants returned by the Views API."
-    """
+    async with aiohttp.ClientSession(headers=headers) as session:
+      async with session.get(views_request_url) as response:
+          response_data = await response.text()
+          parsed_data = parse_participants(response_data)
+          response = Response(data=parsed_data, status=200)
+          response.accepted_renderer= JSONRenderer()
+          response.accepted_media_type = 'application/json'
+          response.renderer_context = {}
+          return response
 
-    if ids != None:
-        id_filter_string = ""
-        for id in ids:
-            id_filter_string += "PersonID[]={}".format(id) +"&"
-
-        response = requests.get(
-            participants_base_url + "search?" + id_filter_string,
-            headers=headers
-        )
-
-    else:
-        if limit != None and offset != None:
-            response = requests.get(
-                participants_base_url
-                + "search?q=&pageFold="
-                + str(limit)
-                + "&offset="
-                + str(offset),
-                headers=headers
-            )
-        else:
-            response = requests.get(
-                participants_base_url + "search?q=",
-                headers=headers
-            )
-
-    return parse_participants(response)
 
 def get_participant_by_id(id, headers):
     headers["Accept"] = "application/json"
@@ -117,7 +90,7 @@ def get_participant_by_id(id, headers):
 
 def parse_participants(response):
     # Remove invalid tags
-    response_text = response.text.replace(
+    response_text = response.replace(
         "<2Personrelationshipandcontactnumberofpersonauthorised_P_229/>", ""
     )
     decoded = response_text.encode("utf-8").decode("unicode_escape").strip('\"')
